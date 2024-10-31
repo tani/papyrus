4439d25fd27a3d4ebe185345dfd13c31995fd26d
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-10-31 14:26:55 +0900

    feat(reader): swap org-reader and md-reader functions
    Swapped the functionality of org-reader and md-reader functions to handle
    different code block formats. Updated the conditions to detect the start
    and end of code blocks in both functions. This change ensures that the
    correct reader function is used for the respective file format.

--- src/reader.lisp
+++ src/reader.lisp
@@ -9,6 +9,16 @@
   (flet ((nil-to-inf (x) (or x most-positive-fixnum)))
     (apply #'< (mapcar #'nil-to-inf args))))
 
+(defun md-reader (s a)
+  (declare (ignore a))
+  (do ((line (read-line s nil nil) (read-line s nil nil))
+       (buffer nil)
+       (codeblock nil))
+      ((not line) (read-from-string (format nil "(progn ~{~a~%~})" (reverse buffer))))
+    (cond ((<? (search "```lisp" line) 1) (setq codeblock t))
+          ((<? (search "```" line) 1) (setq codeblock nil))
+          (codeblock (push line buffer)))))
+
 (defun org-reader (s a)
   (declare (ignore a))
   (do ((line (read-line s nil nil) (read-line s nil nil))
@@ -18,19 +28,6 @@
     (cond ((<? (search "#+BEGIN_SRC" line) 1 (search "lisp" line) (search ":tangle" line) (search "yes" line) most-positive-fixnum)
            (setq codeblock t))
           ((<? (search "#+END_SRC" line) 1) (setq codeblock nil))
-          (codeblock (push line buffer)))))
-
-(defun md-reader (s a)
-  (declare (ignore a))
-  (do ((line (read-line s nil nil) (read-line s nil nil))
-       (buffer nil)
-       (codeblock nil))
-      ((not line)
-       (progn
-         (print (reverse buffer))
-         (read-from-string (format nil "(progn ~{~a~%~})" (reverse buffer)))))
-    (cond ((<? (search "```lisp" line) 1) (setq codeblock t))
-          ((<? (search "```" line) 1) (setq codeblock nil))
           (codeblock (push line buffer)))))
 
 (defun pod-reader (s a)
